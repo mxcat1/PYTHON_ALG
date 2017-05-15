ae565ca5583c869bec53370ec17a3f405343d9b7
CHRISTIAN NICOLL VILCA APAZA <c.vilca.apaza@isur.edu.pe>
2017-05-14 23:46:07 -0500

    tarea luigui ejercicio 4

--- cursopython35/TAREA_LUIGUI/4A.py
+++ cursopython35/TAREA_LUIGUI/4A.py
@@ -3,6 +3,30 @@ Created on 14 may. 2017
 
 @author: MXCATEX
 '''
-import tkinter
-from tkinter import *
+import sys
+import PyQt5
+from PyQt5 import *
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+
+#Clase heredada de QmainWindow(Constructor de Ventanas)
+
+class Ventana(QMainWindow):
+    #Metodo Constructor de la clase
+    def __init__(self):
+        #Iniciar el objeto QMainWindow
+        QMainWindow.__init__(self)
+        #Cargar la configuracion del archivo uic
+        uic.loadUi("MainWindows.ui",self)
+        
+#instancia para iniciar una aplicacion
+app=QApplication(sys.argv)
+#Creacion de un objeto de la clase
+_ventana=Ventana()
+#Mostrar Ventana
+_ventana.show()
+#ejecutar la aplicacion
+app.exec_()
+    
 
