7a84e37e6e1854532ce676e6deb41c3ae2e77c63
MAXCAT <MAXCAT@DESKTOP-1CCA8OP>
2017-05-19 21:11:28 -0500

    hoy papu Change-Id: Icd6bd734d76a10651e6542f4d6986f176e0c4c58

--- cursopython35/TAREA_LUIGUI/EJERCICIO11MY.py
+++ cursopython35/TAREA_LUIGUI/EJERCICIO11MY.py
@@ -3,13 +3,41 @@ Created on 19 may. 2017
 
 @author: MAXCAT
 '''
+#!/urs/bin/env python
+# -*- coding: utf-8-*-
 import tkinter
-import TAREA_LUIGUI.clase-contacto
+import sqlite3
 from tkinter import *
+from TAREA_LUIGUI.persona import persona
+from sqlite3 import *
 
 persona1=persona()
 
+def ingresar():
+    cone=sqlite3.connect("contacto.db3")
+    cmd=cone.cursor()
+    persona1.ingreso("Pedro", "Vilca", "CALLE 1", "15-03-17")
+    reg=(3,persona1.nombre,persona1.apellido,persona1.fecha)
+    cmd.execute("INSERT INTO contacto VALUES(?,?,?,?)",reg)
+    cone.commit()
 
+#ingresar()
+    
+'''cone=sqlite3.connect('contacto.db3')
+cmd=cone.cursor()
+print("SE ABRIO CORRECTAMENTE")
+
+cmd.execute("select * from contacto")
+
+for registro in cmd:
+    print(registro[0])
+    print(registro[1])
+    print(registro[2])
+    print(registro[3])
+'''
 
 ventana=Tk()
+ventana.title("AGENDA")
+ventana.geometry("400x300")
+lblnombre=Label(ventana,text="NOMBRE").place(x=10,y=10)
 ventana.mainloop()
---
+++ cursopython35/TAREA_LUIGUI/persona.py
@@ -0,0 +1,23 @@
+'''
+Created on 19 may. 2017
+
+@author: MAXCAT
+'''
+class persona():
+    fecha=""
+    nombre=""
+    apellido=""
+    direccion=""
+    
+    def __init__(self):
+        print("")
+        
+    def ingreso(self,n,a,d,fecha):
+        self.nombre=n
+        self.apellido=a
+        self.direccion=d
+        self.fecha=fecha
+        
+        
+        
+        
